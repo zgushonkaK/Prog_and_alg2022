227f8cfc210e5e7a4323f109d5b58dbcfaba036d
vsevolod <vsevolod.gor@yandex.ru>
2022-11-23 11:39:15 +0300

    23.11.2022

--- gorokhovatskii_v_m/vectora/main.cpp
+++ gorokhovatskii_v_m/vectora/main.cpp
@@ -10,52 +10,6 @@ struct Rpol2D{
     double r;
     double a;
 };
-
-std::ostream& operator<<(std::ostream& out, const Rdec2D& c){
-    return out << '(' << c.x << ',' << c.y << ')';
-}
-std::ostream& operator<<(std::ostream& out, const Rpol2D& c){
-    return out << '(' << c.r << ',' << c.a << ')';
-}
-Rdec2D operator+(Rdec2D& rhs, Rdec2D& lhs){
-    return {rhs.x + lhs.x, rhs.y + lhs.y};
-}
-Rpol2D operator+(Rpol2D& rhs, Rpol2D& lhs){
-    return {ToPol(ToDec(rhs) + ToDec(lhs))};
-}
-Rdec2D operator-(Rdec2D& rhs, Rdec2D& lhs){
-    return {rhs.x - lhs.x, rhs.y - lhs.y};
-}
-Rpol2D operator-(Rpol2D& rhs, Rpol2D& lhs){
-    return {ToPol(ToDec(rhs) - ToDec(lhs))};
-}
-Rdec2D operator-(Rdec2D& rhs){
-    return {(-1)*rhs.x, (-1)*rhs.y};
-}
-Rdec2D operator*(Rdec2D& rhs, double& a){
-    return {rhs.x * a, rhs.y * a};
-}
-Rpol2D operator*(Rpol2D& rhs, Rpol2D& lhs){
-    return {ToPol(ToDec(rhs) * ToDec(lhs))};
-}
-Rdec2D operator/(Rdec2D& rhs, double& a){
-    return {rhs.x / a, rhs.y / a};
-}
-Rpol2D operator/(Rpol2D& rhs, Rpol2D& lhs){
-    return {ToPol(ToDec(rhs) / ToDec(lhs))};
-}
-double dot(Rdec2D& rhs, Rdec2D& lhs){
-    return {rhs.x * lhs.x + rhs.y * lhs.y};
-}
-double norm(Rdec2D& rhs){
-    return {sqrt(pow(rhs.x, 2) + pow(rhs.y, 2))};
-}
-Rdec2D operator==(Rdec2D& rhs, Rdec2D& lhs){
-    return {(rhs.x == lhs.x) && (rhs.y == lhs.y)};
-}
-Rdec2D operator!=(Rdec2D& rhs, Rdec2D& lhs){
-    return {!((rhs.x == lhs.x) && (rhs.y == lhs.y))};
-}
 Rpol2D ToPol(Rdec2D& rhs){
     Rpol2D temp;
     temp.r = sqrt(pow(rhs.x, 2) + pow(rhs.y, 2));
@@ -68,6 +22,74 @@ Rdec2D ToDec(Rpol2D& rhs){
     temp.y = rhs.r * sin(rhs.a);
     return temp;
 }
+std::ostream& operator<<(std::ostream& out, const Rdec2D& c){
+    return out << '(' << c.x << ',' << c.y << ')';
+}
+std::ostream& operator<<(std::ostream& out, const Rpol2D& c){
+    return out << '(' << c.r << ',' << c.a << ')';
+}
+Rdec2D operator+(Rdec2D& rhs, Rdec2D& lhs){
+    return {rhs.x + lhs.x, rhs.y + lhs.y};
+}
+//Rpol2D operator+(Rpol2D& rhs, Rpol2D& lhs){
+    //return {ToPol(ToDec(rhs) + ToDec(lhs))};
+//}
+Rdec2D operator-(Rdec2D& rhs, Rdec2D& lhs){
+    return {rhs.x - lhs.x, rhs.y - lhs.y};
+}
+//Rpol2D operator-(Rpol2D& rhs, Rpol2D& lhs){
+    //return {ToPol(ToDec(rhs) - ToDec(lhs))};
+//}
+Rdec2D operator-(Rdec2D& rhs){
+    return {(-1)*rhs.x, (-1)*rhs.y};
+}
+Rdec2D operator*(Rdec2D& rhs, double& a){
+    return {rhs.x * a, rhs.y * a};
+}
+//Rpol2D operator*(Rpol2D& rhs, Rpol2D& lhs){
+    //return {ToPol(ToDec(rhs) * ToDec(lhs))};
+//}
+Rdec2D operator/(Rdec2D& rhs, double& a){
+    return {rhs.x / a, rhs.y / a};
+}
+//Rpol2D operator/(Rpol2D& rhs, Rpol2D& lhs){
+    //return {ToPol(ToDec(rhs) / ToDec(lhs))};
+//}
+double dot(Rdec2D& rhs, Rdec2D& lhs){
+    return {rhs.x * lhs.x + rhs.y * lhs.y};
+}
+//double dot(Rpol2D& rhs, Rpol2D& lhs){
+    //Rdec2D temp_rhs, temp_lhs;
+    //temp_rhs = ToDec(rhs);
+    //temp_lhs = TOdec(lhs);
+    //return {temp_rhs.x * temp_lhs.x + temp_rhs.y * temp_lhs.y};
+//}
+double norm(Rdec2D& rhs){
+    return {sqrt(pow(rhs.x, 2) + pow(rhs.y, 2))};
+}
+//double norm(Rpol2D& rhs){
+    //Rdec2D temp_rhs;
+    //temp_rhs = ToDec(rhs);
+    //return {sqrt(pow(temp_rhs.x, 2) + pow(temp_rhs.y, 2))};
+//}
+bool operator==(Rdec2D& rhs, Rdec2D& lhs){
+    return {(rhs.x == lhs.x) && (rhs.y == lhs.y)};
+}
+//bool operator==(Rpol2D& rhs, Rpol2D& lhs){
+    //Rdec2D temp_rhs, temp_lhs;
+    //temp_rhs = ToDec(rhs);
+    //temp_lhs = ToDec(lhs);
+    //return {(temp_rhs.x == temp_lhs.x) && (temp_rhs.y == temp_lhs.y)};
+//}
+bool operator!=(Rdec2D& rhs, Rdec2D& lhs){
+    return {!((rhs.x == lhs.x) && (rhs.y == lhs.y))};
+}
+//bool operator!=(Rpol2D& rhs, Rpol2D& lhs){
+    //Rdec2D temp_rhs, temp_lhs;
+    //temp_rhs = ToDec(rhs);
+    //temp_lhs = ToDec(lhs);
+    //return {!((temp_rhs.x == temp_lhs.x) && (temp_rhs.y == temp_lhs.y))};
+//}
 int main(){
     Rdec2D c{4.0, 3.0};
     Rpol2D x;
