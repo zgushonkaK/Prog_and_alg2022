bd9933f913910a08a5a42778c2e5918e0a81f0a5
zgushonkaK <vsevolod.gor@yandex.ru>
2022-12-07 19:18:16 +0300

    7/12/2022

--- gorokhovatskii_v_m/1699.cpp
+++ gorokhovatskii_v_m/1699.cpp
@@ -1,30 +1,30 @@
-    #include<iostream>
-    int main() {
-        int t;
-        std::cin >> t;
-        while (t--) {
-            int n;
-            int m;
-            std::cin >> n >> m;
-            for (int i = 0; i < n; i += 1) {
-                for (int j = 0; j < m; j += 1) {
-                    if (i % 4 == 0 || i % 4 == 3)
-                    {
-                        if (j % 4 == 0 || j % 4 == 3)
-                            std::cout << 1 << " ";
-                        else
-                            std::cout << 0 << " ";
-                    }
+#include<iostream>
+int main() {
+    int t;
+    std::cin >> t;
+    while (t--) {
+        int n;
+        int m;
+        std::cin >> n >> m;
+        for (int i = 0; i < n; i += 1) {
+            for (int j = 0; j < m; j += 1) {
+                if (i % 4 == 0 || i % 4 == 3)
+                {
+                    if (j % 4 == 0 || j % 4 == 3)
+                        std::cout << 1 << " ";
                     else
-                    {
-                        if (j % 4 == 0 || j % 4 == 3)
-                            std::cout << 0 << " ";
-                        else
-                            std::cout << 1 << " ";
-                    }
+                        std::cout << 0 << " ";
                 }
-                std::cout << std::endl;
+                else
+                {
+                    if (j % 4 == 0 || j % 4 == 3)
+                        std::cout << 0 << " ";
+                    else
+                        std::cout << 1 << " ";
+                }
             }
+            std::cout << std::endl;
         }
-        return 0;
     }
+    return 0;
+}
