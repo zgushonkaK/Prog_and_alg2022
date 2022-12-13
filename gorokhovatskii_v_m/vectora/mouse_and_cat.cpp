28ed76dd1c8f8a069f33f1657f175ba3ebeba842
zgushonkaK <vsevolod.gor@yandex.ru>
2022-12-13 21:55:06 +0300

    fixed geometry tasks

--- gorokhovatskii_v_m/vectora/mouse_and_cat.cpp
+++ gorokhovatskii_v_m/vectora/mouse_and_cat.cpp
@@ -1,32 +1,54 @@
-#include "veclibrary.hpp"
 #include <iostream>
-void NormVec(Rdec2D& vec) {
-	vec = vec / Norm(vec);
+#include <fstream>
+#include "geom.hpp"
+
+void Life_or_death(const double& dt, const double& mod_u_m_start, Rdec2D& r_m, const double& mod_u_c_start, Rdec2D& r_c, const double& r_kill) {
+    std::ofstream file("data.txt");
+    Rdec2D u_m = (Rdec2D{ 0.0, 0.0 } - r_m) / norm(Rdec2D{ 0.0, 0.0 } - r_m) * mod_u_m_start;
+    Rdec2D dr_m;
+    Rdec2D past_r_m = r_m;
+
+    Rdec2D u_c = (r_m - r_c) / norm(r_m - r_c) * mod_u_c_start;
+    Rdec2D dr_c;
+    Rdec2D past_r_c = r_c;
+
+    for (int t = 0; t < 1000; t += dt) {
+        u_m = (Rdec2D{ 0.0, 0.0 } - r_m) / norm(Rdec2D{ 0.0, 0.0 } - r_m) * mod_u_m_start;
+        dr_m = u_m * dt;
+        r_m = r_m + dr_m;
+
+        u_c = (r_m - r_c) / norm(r_m - r_c) * mod_u_c_start;
+        dr_c = u_c * dt;
+        r_c = r_c + dr_c;
+
+        if (r_m.y >= 0) {
+            std::cout << "mouse won" << std::endl <<  "time:" << t;
+            break;
+        }
+        else {
+            if (norm(r_m - r_c) <= r_kill) {
+                std::cout << "mouse died" << std::endl <<  "time:" << t;
+                break;
+            }
+            else {
+                if (norm(r_m - r_c) > norm(past_r_m - past_r_c)) {
+                    std::cout << "mouse will win" << std::endl <<  "time:" << t;
+                    break;
+                }
+            }
+        }
+        past_r_m = r_m;
+        past_r_c = r_c;
+    }
 }
 
-int TimeToCatch(const Rdec2D cat_pos, const Rdec2D mouse_pos, 
-				const Rdec2D cat_speed, const Rdec2D mouse_speed,
-				const double cat_range) {
-
-	Rdec2D cat_dir = cat_pos;
-  NormVec(cat_dir);
-	Rdec2D mouse_dir = mouse_pos;
-	NormVec(mouse_dir);
-
-	int time = 0;
-
-	while (Norm(cat_pos - mouse_pos) > cat_range) {
-		cat_dir = cat_dir + cat_speed;
-		mouse_dir = mouse_dir + mouse_speed;
-		time += 1;
-	}
-
-	return time;
-
-}
 int main(){
-    Rdec2D r_c{5.0, 0};
-    Rdec2D r_m{0, 3.0};
-    Rdec2D v_m{0, 1.0};
-    
+    double dt = 1;
+    double mod_v_m = 0.5;
+    double mod_v_c = 0.3;
+    Rdec2D r_m = {0, -10.0};
+    Rdec2D r_c = {8.0, 0};
+    double kill_area = 0.2;
+    Life_or_death(dt, mod_v_m, r_m, mod_v_c, r_c, kill_area);
+    return 0;
 }
